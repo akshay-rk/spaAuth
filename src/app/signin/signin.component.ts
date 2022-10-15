599cef74650db50427080656f2b17ce4fcad6078
akshay-rk <akshayrkofficial@gmail.com>
2022-10-15 17:01:46 +0530

    c

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  signinform! : FormGroup
+  constructor(private formbuilder:FormBuilder, private http:HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+    this.signinform=this.formbuilder.group(
+      {
+        name: [''],
+        password:[''],
+        // gender:[''],
+        // state:[''],
+        // phone:['']
+      }
+    )
+  }
+  signin() {
+    this.http.get<any>('http://localhost:3000/user')
+    .subscribe( res=>{ 
+      const user=res.find((a:any)=>{
+        return a.name===this.signinform.value.name
+          a.password===this.signinform.value.password
+
+      })
+      if(user){
+        alert("Welcome, Your authentication is successful");
+        this.signinform.reset();
+      }
+      else{
+        alert("Welcome, Your authentication is failed");
+      }
+      
+     } )
   }
 
 }
